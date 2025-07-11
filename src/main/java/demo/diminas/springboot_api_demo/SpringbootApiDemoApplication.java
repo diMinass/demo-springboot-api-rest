87e4a2a638a45c013fc8295c8b6329db45504dec
diMinass <luiz.gus.ms@gmail.com>
2025-07-11 17:56:50 -0300

    primeiro commit

---
+++ src/main/java/demo/diminas/springboot_api_demo/SpringbootApiDemoApplication.java
@@ -0,0 +1,13 @@
+package demo.diminas.springboot_api_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/diminas/springboot_api_demo/SpringbootApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.diminas.springboot_api_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
